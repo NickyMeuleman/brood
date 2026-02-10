9fcb34e40e7351c53f52e231400089cdd99ca32d
Nicky Meuleman <nicky.dev@outlook.com>
2026-02-10 19:03:46 +0100

    add shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
-import "./styles/index.css";
+import "@/styles/global.css";
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
