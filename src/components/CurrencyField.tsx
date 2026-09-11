9adb416b5f249d5a067541f360ee4de9028d7337
Nicky Meuleman <nicky.dev@outlook.com>
2026-09-11 16:07:11 +0200

    reuse backend currencyinput type

--- src/components/CurrencyField.tsx
+++ src/components/CurrencyField.tsx
@@ -1,3 +1,4 @@
+import type { MoneyInput } from "@/bindings";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import {
 	InputGroup,
@@ -17,11 +18,6 @@ import { useFieldContext } from "@/hooks/form-context";
 import { useCurrencies } from "@/hooks/use-currencies";
 import { getCurrencySymbol } from "@/lib/utils";
 
-export type CurrencyValue = {
-	currencyCode: string;
-	amount: string;
-};
-
 export const CurrencyField = ({
 	label,
 	initialCurrencyCode = "EUR",
@@ -31,11 +27,11 @@ export const CurrencyField = ({
 	initialCurrencyCode?: string;
 	currencyDisabled?: boolean;
 }) => {
-	const field = useFieldContext<CurrencyValue>();
+	const field = useFieldContext<MoneyInput>();
 	const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
 	const { data: currencies = [] } = useCurrencies();
 
-	const currency = field.state.value?.currencyCode ?? initialCurrencyCode;
+	const currency = field.state.value?.currency ?? initialCurrencyCode;
 	const amount = field.state.value?.amount ?? "";
 
 	return (
@@ -58,7 +54,7 @@ export const CurrencyField = ({
 						onChange={(e) => {
 							const val = e.target.value;
 							if (/^-?\d*\.?\d*$/.test(val)) {
-								field.handleChange({ currencyCode: currency, amount: val });
+								field.handleChange({ currency, amount: val });
 							}
 						}}
 						onBlur={(e) => {
@@ -71,7 +67,7 @@ export const CurrencyField = ({
 
 							if (cleanVal !== e.target.value) {
 								field.handleChange({
-									currencyCode: currency,
+									currency,
 									amount: cleanVal,
 								});
 							}
@@ -86,10 +82,10 @@ export const CurrencyField = ({
 				<Select
 					id={currency}
 					value={currency}
-          disabled={currencyDisabled}
-					onValueChange={(code) => {
-						if (code) {
-							field.handleChange({ currencyCode: code, amount });
+					disabled={currencyDisabled}
+					onValueChange={(currency) => {
+						if (currency) {
+							field.handleChange({ currency, amount });
 						}
 					}}
 					onOpenChange={(open) => {
